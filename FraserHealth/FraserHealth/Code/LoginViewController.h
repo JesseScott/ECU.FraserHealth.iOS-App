091bd5383663b19d33100827ca6b4ef85b8c79bd
Jesse Scott <jessecolinscott@gmail.com>
2014-03-06 14:21:25 -0800

    Login VC
    Former-commit-id: 319271de4716fcf5602e4307a051727249d1eda0

---
+++ FraserHealth/FraserHealth/Code/LoginViewController.h
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.h
+//  FraserHealth
+//
+//  Created by Jesse Scott on 2014-03-06.
+//  Copyright (c) 2014 ECUAD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController {
+    
+}
+
+// Properties
+
+@property (nonatomic, strong) UIPopoverController* buttonPopoverController;
+
+
+@end
